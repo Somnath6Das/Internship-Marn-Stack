606c12aed76d920f65812d52b6c25e6f6edea2ac
Somnath Das <98904596+Somnath6Das@users.noreply.github.com>
2023-03-30 11:36:27 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/headers/Headers';
+import Home from './pages/home/Home';
+import Register from './pages/register/Register';
+import Edit from './pages/edit/Edit';
+import Profile from './pages/profile/Profile';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     hello world
+      <Header />
+      <Routes>
+        <Route path='/' element={ <Home/> } />
+        <Route path='/register' element={ <Register/> } />
+        <Route path='/edit/:id' element={ <Edit/> } />
+        <Route path='/userprofile/:id' element={ <Profile/> } />
+      </Routes>
     </div>
   );
 }
--- client/src/pages/register/Register.js
+++ client/src/pages/register/Register.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import "./register.css";
+import Card from 'react-bootstrap/Card';
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="register">
+      <h2 className='text-center mt-1'>Register Your Details</h2>
+      <Card className='shadow mt-3 p-3'>
+        <div className="profile_div text-center">
+
+        </div>
+      </Card>
+    </div>
   )
 }
 
