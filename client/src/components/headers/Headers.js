8ba00b1e0e440ba5dc7a279c3c27bf1a754788f1
Somnath Das <98904596+Somnath6Das@users.noreply.github.com>
2023-03-31 10:44:56 +0530

    update

--- client/src/components/headers/Headers.js
+++ client/src/components/headers/Headers.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Headers = () => {
   return (
-    <div>Headers</div>
+    <div className="headers">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          {/* <Navbar.Brand href="#home">Navbar</Navbar.Brand> */}
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>        
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
   )
 }
 
--- client/src/pages/register/Register.js
+++ client/src/pages/register/Register.js
@@ -1,15 +1,78 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import "./register.css";
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Select from 'react-select';
+
 
 const Register = () => {
+  // status options
+  const options = [
+    { value: 'Active', label: 'Active' },
+    { value: 'Inactive', label: 'Inactive' },
+  ];
+
   return (
     <div className="register">
-      <h2 className='text-center mt-1'>Register Your Details</h2>
+      <h2 className='text-center mt-1'>Register Details</h2>
       <Card className='shadow mt-3 p-3'>
         <div className="profile_div text-center">
-
+          <img src="/selfie.png" alt="img" />
         </div>
+        <Form>
+          <Row>
+            <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+              <Form.Label>First name</Form.Label>
+              <Form.Control type="text" name='fname' placeholder="Enter first name" />
+            </Form.Group>
+            <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+              <Form.Label>Last name</Form.Label>
+              <Form.Control type="text" name='fname' placeholder="Enter last name" />
+            </Form.Group>
+            <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" name='email' placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+              <Form.Label>Mobile number</Form.Label>
+              <Form.Control type="text" placeholder="Enter mobile" />
+            </Form.Group>
+            <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+              <Form.Label>Select Gender</Form.Label>
+              <Form.Check
+                type={'radio'}
+                label={`Male`}
+                name="gender"
+                value={"Male"}
+              />
+              <Form.Check
+                type={'radio'}
+                label={`Female`}
+                name="gender"
+                value={"Female"}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+              <Form.Label>User status</Form.Label>
+              <Select
+                options={options}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+              <Form.Label>Select profile picture</Form.Label>
+              <Form.Control type="file" name='user_profile' placeholder="Select picture" />
+            </Form.Group>
+            <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+              <Form.Label>Enter location</Form.Label>
+              <Form.Control type="text" name='location' placeholder="Enter location" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Row>
+        </Form>
       </Card>
     </div>
   )
