3d511634295ed3facc3effec104279c25a686fe9
Somnath Das <98904596+Somnath6Das@users.noreply.github.com>
2023-04-02 01:24:06 +0530

    updated

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import "./home.css";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
+
 
 const Home = () => {
   return (
@@ -9,8 +11,8 @@ const Home = () => {
       <div className="main_div">
         <div className="search_add mt-4 d-flex justify-content-between" >
           <div className="search col-lg-4">
-            <Form className="d-flex" style={{marginLeft: "124px", maxWidth:"300px"}}>
-              <Form.Control 
+            <Form className="d-flex" style={{ marginLeft: "124px", maxWidth: "300px" }}>
+              <Form.Control
                 type="search"
                 placeholder="Search user name"
                 className="me-2"
@@ -20,40 +22,75 @@ const Home = () => {
             </Form>
           </div>
           <div className="add_btn">
-            <Button variant="primary"><i class="fa-solid fa-user-plus"></i>&nbsp;&nbsp;<span>Add User</span></Button>
+            <Button variant="primary"><i class="fa-solid fa-user-plus"></i>&nbsp;&nbsp;Add User</Button>
           </div>
         </div>
         <div className="filter_div mt-5 d-flex justify-content-between flex-wrap">
+        <div >
+          <Dropdown >
+            <Dropdown.Toggle className="filter_new_old" id="dropdown-basic">
+            <i class="fa-solid fa-arrow-down-z-a"></i>&nbsp;
+            Short by Value
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item className='menu'>New to Old</Dropdown.Item>
+              <Dropdown.Item className='menu'>Old to New</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          </div>
+          
+          <div className="filter">
+            <h5>Filter By Gender</h5>
+            <div className="gender d-flex justify-content-between" >
+              <Form.Check style={{ marginRight: "20px" }}
+                type={'radio'}
+                label={`All`}
+                name="gender"
+                value={"All"}
+                defaultChecked
+              />
+              <Form.Check style={{ marginRight: "20px" }}
+                type={'radio'}
+                label={`Male`}
+                name="gender"
+                value={"Male"}
+              />
+              <Form.Check style={{ marginRight: "20px" }}
+                type={'radio'}
+                label={`Female`}
+                name="gender"
+                value={"Female"}
+              />
+            </div>
+          </div>
+         
+          <div className="filter">
+            <h5>Filter By Status</h5>
+            <div className="gender d-flex justify-content-between" >
+              <Form.Check style={{ marginRight: "20px" }}
+                type={'radio'}
+                label={`All`}
+                name="status"
+                value={"All"}
+                defaultChecked
+              />
+              <Form.Check style={{ marginRight: "20px" }}
+                type={'radio'}
+                label={`Active`}
+                name="status"
+                value={"Active"}
+              />
+              <Form.Check style={{ marginRight: "20px" }}
+                type={'radio'}
+                label={`Inactive`}
+                name="status"
+                value={"Inactive"}
+              />
+            </div>
+          </div>
           <div className="export_csv">
             <Button className='export_btn'><i class="fa-solid fa-file-csv"></i>&nbsp;&nbsp;<span>Save to Excel File</span></Button>
           </div>
-        
-            <div className="filter">
-              <h4>Filter By Gender</h4>
-              <div className="gender d-flex justify-content-between" >
-              <Form.Check style={{marginRight: "20px"}}
-                  type={'radio'}
-                  label={`All`}
-                  name="gender"
-                  value={"Male"}
-                  defaultChecked
-                />
-              <Form.Check style={{marginRight: "20px"}}
-                  type={'radio'}
-                  label={`Male`}
-                  name="gender"
-                  value={"Male"}
-                 
-                />
-              <Form.Check style={{marginRight: "20px"}}
-                  type={'radio'}
-                  label={`Female`}
-                  name="gender"
-                  value={"Female"}                  
-                />
-              </div>
-            </div>
-          
         </div>
       </div>
     </div>
